8068dc91422eb3f33ae88807e2fe6138f33380b0
Rain <rain1@fb.com>
2020-06-30 13:15:58 -0700

    [proptest-macro] add some docs

--- propfuzz-macro/src/lib.rs
+++ propfuzz-macro/src/lib.rs
@@ -3,7 +3,8 @@
 
 //! Procedural macros for propfuzz tests.
 //!
-//! This library is **not** meant to be used directly. Use it through the `propfuzz` library.
+//! This crate is an implementation detail of `propfuzz` and is **not** meant to be used directly.
+//! Use it through [`propfuzz`](https://crates.io/crates/propfuzz) instead.
 
 extern crate proc_macro;
 
@@ -14,7 +15,71 @@ mod config;
 mod errors;
 mod propfuzz_impl;
 
-/// The main propfuzz macro.
+/// The core macro, used to annotate test methods.
+///
+/// Annotate a function with this in order to have it run as a property-based test using the
+/// [`proptest`](https://docs.rs/proptest) framework. In the future, such tests will also be
+/// available as fuzz targets.
+///
+/// # Examples
+///
+/// ```
+/// // The prelude imports the `propfuzz` macro.
+/// use propfuzz::prelude::*;
+/// use proptest::collection::vec;
+///
+/// /// Reversing a list twice produces the same result.
+/// #[propfuzz]
+/// fn reverse(
+///     #[strategy(vec(any::<u32>(), 0..64))]
+///     mut list: Vec<u32>,
+/// ) {
+///     let list2 = list.clone();
+///     list.reverse();
+///     list.reverse();
+///     prop_assert_eq!(list, list2);
+/// }
+/// ```
+///
+/// # Arguments
+///
+/// `propfuzz` supports a number of arguments which can be used to customize test behavior.
+///
+/// Attributes can be broken up with commas and split up across multiple lines like so:
+///
+/// ```
+/// use propfuzz::prelude::*;
+/// use proptest::collection::vec;
+///
+/// /// Options broken up across multiple lines.
+/// #[propfuzz(cases = 1024, max_local_rejects = 10000)]
+/// #[propfuzz(fork = true)]
+/// fn reverse(
+///     #[strategy(vec(any::<u32>(), 0..64))]
+///     mut list: Vec<u32>,
+/// ) {
+///     let list2 = list.clone();
+///     list.reverse();
+///     list.reverse();
+///     prop_assert_eq!(list, list2);
+/// }
+/// ```
+///
+/// ## Proptest configuration
+///
+/// The following `proptest`
+/// [configuration options](https://docs.rs/proptest/0.10/proptest/test_runner/struct.Config.html)
+/// are supported:
+///
+/// * `cases`
+/// * `max_local_rejects`
+/// * `max_global_rejects`
+/// * `max_flat_map_regens`
+/// * `fork`
+/// * `timeout`
+/// * `max_shrink_time`
+/// * `max_shrink_iters`
+/// * `verbose`
 #[proc_macro_attribute]
 pub fn propfuzz(attr: TokenStream, item: TokenStream) -> TokenStream {
     let attr = parse_macro_input!(attr as AttributeArgs);
