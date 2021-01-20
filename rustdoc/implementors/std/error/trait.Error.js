807c06f18bc13ec593e7d2881115f3afd6da33d5
sunshowers <rain@sunshowers.io>
2021-01-20 21:02:15 +0000

    Deploying to gh-pages from  @ 1316197e671715a7427205eec6f99f94a5b72bfd 🚀

--- rustdoc/implementors/std/error/trait.Error.js
+++ rustdoc/implementors/std/error/trait.Error.js
@@ -1,5 +1,6 @@
 (function() {var implementors = {};
 implementors["getrandom"] = [{"text":"impl Error for Error","synthetic":false,"types":[]}];
+implementors["proc_macro2"] = [{"text":"impl Error for LexError","synthetic":false,"types":[]}];
 implementors["proptest"] = [{"text":"impl&lt;T:&nbsp;Debug&gt; Error for TestError&lt;T&gt;","synthetic":false,"types":[]},{"text":"impl Error for Error","synthetic":false,"types":[]}];
 implementors["rand"] = [{"text":"impl Error for BernoulliError","synthetic":false,"types":[]},{"text":"impl Error for WeightedError","synthetic":false,"types":[]},{"text":"impl Error for ReadError","synthetic":false,"types":[]}];
 implementors["rand_core"] = [{"text":"impl Error for Error","synthetic":false,"types":[]}];
