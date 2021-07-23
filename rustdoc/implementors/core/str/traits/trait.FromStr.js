69e23ec321d7beeaef61e8b3c5c6710674476628
sausagee <youngyl@fb.com>
2021-07-23 19:18:56 +0000

    Deploying to gh-pages from  @ df677ad52ec92f277c8db5d757b8c742705fe2ae 🚀

--- rustdoc/implementors/core/str/traits/trait.FromStr.js
+++ rustdoc/implementors/core/str/traits/trait.FromStr.js
@@ -1,4 +1,4 @@
 (function() {var implementors = {};
-implementors["proc_macro2"] = [{"text":"impl FromStr for TokenStream","synthetic":false,"types":[]}];
+implementors["proc_macro2"] = [{"text":"impl FromStr for TokenStream","synthetic":false,"types":[]},{"text":"impl FromStr for Literal","synthetic":false,"types":[]}];
 implementors["proptest"] = [{"text":"impl FromStr for PersistedSeed","synthetic":false,"types":[]},{"text":"impl FromStr for RngAlgorithm","synthetic":false,"types":[]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
